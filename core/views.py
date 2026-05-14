5837fe2ba029011a671c401b869c0014b3c6b796
samuelo1020 <sorozcov@eafit.edu.co>
2026-05-13 22:22:32 -0500

    fix: redirect admin users to home after login instead of /admin/

--- core/views.py
+++ core/views.py
@@ -27,8 +27,6 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            if user.is_superuser:
-                return redirect("/admin/")
             return redirect("home")
 
         error = (
